b634bdd369799cd1fbfbe81f2e056df9e292d312
Qingyi <xqy1115@gmail.com>
2021-11-03 15:25:46 +0800

    created 5 enum classes

---
+++ HotelReservationSystemLibrary/src/util/enumeration/AccessRight.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.enumeration;
+
+/**
+ *
+ * @author xqy11
+ */
+public enum AccessRight {
+    SYSTEM_ADMIN,
+    OPERATION_MANAGER, 
+    SALES_MANAGER,
+    GUEST_RELATION_OFFICER
+}
