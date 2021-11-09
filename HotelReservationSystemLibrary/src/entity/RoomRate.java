bd10f61564610ce3ff739cbd16c57ab56520e561
Qingyi <xqy1115@gmail.com>
2021-11-09 15:29:00 +0800

    created room and roomType entity

---
+++ HotelReservationSystemLibrary/src/entity/RoomRate.java
@@ -0,0 +1,185 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import util.enumeration.RoomRateType;
+
+/**
+ *
+ * @author xqy11
+ */
+@Entity
+public class RoomRate implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomRateId;
+    @Column(nullable = false,length = 64)
+    @NotNull
+    @Size(min = 1, max = 64)
+    private String name;
+    @Column(nullable = false)
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RoomRateType rateType;
+    @Column(nullable = false, precision = 11, scale = 2)
+    @NotNull
+    @DecimalMin("0.00")
+    @Digits(integer = 9, fraction = 2) // 11 - 2 digits to the left of the decimal point
+    private BigDecimal ratePerNight;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = true)
+    private Date startDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = true)
+    private Date endDate;
+    @Column(nullable = false)
+    @NotNull
+    private Boolean disabled;
+    
+//    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+//    @JoinColumn(nullable = false)
+//    private RoomType roomType;
+    
+
+    public Long getRoomRateId() {
+        return roomRateId;
+    }
+
+    public void setRoomRateId(Long roomRateId) {
+        this.roomRateId = roomRateId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (roomRateId != null ? roomRateId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the roomRateId fields are not set
+        if (!(object instanceof RoomRate)) {
+            return false;
+        }
+        RoomRate other = (RoomRate) object;
+        if ((this.roomRateId == null && other.roomRateId != null) || (this.roomRateId != null && !this.roomRateId.equals(other.roomRateId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.RoomRate[ id=" + roomRateId + " ]";
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the rateType
+     */
+    public RoomRateType getRateType() {
+        return rateType;
+    }
+
+    /**
+     * @param rateType the rateType to set
+     */
+    public void setRateType(RoomRateType rateType) {
+        this.rateType = rateType;
+    }
+
+    /**
+     * @return the ratePerNight
+     */
+    public BigDecimal getRatePerNight() {
+        return ratePerNight;
+    }
+
+    /**
+     * @param ratePerNight the ratePerNight to set
+     */
+    public void setRatePerNight(BigDecimal ratePerNight) {
+        this.ratePerNight = ratePerNight;
+    }
+
+    /**
+     * @return the startDate
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * @param startDate the startDate to set
+     */
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     * @return the endDate
+     */
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * @param endDate the endDate to set
+     */
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    /**
+     * @return the disabled
+     */
+    public Boolean getDisabled() {
+        return disabled;
+    }
+
+    /**
+     * @param disabled the disabled to set
+     */
+    public void setDisabled(Boolean disabled) {
+        this.disabled = disabled;
+    }
+    
+}
